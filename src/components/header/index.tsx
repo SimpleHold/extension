2e5b8ae36636715b308504770c64e70dda2ee9f3
Lucas Root <lucas@simpleswap.io>
2021-02-15 18:06:29 +0300

    feat: add prettier eslint

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,22 +3,22 @@ import { Link } from 'preact-router/match';
 import style from './style.css';
 
 const Header: FunctionalComponent = () => {
-    return (
-        <header class={style.header}>
-            <h1>Preact App</h1>
-            <nav>
-                <Link activeClassName={style.active} href="/">
-                    Home
+  return (
+    <header class={style.header}>
+      <h1>Preact App</h1>
+      <nav>
+        <Link activeClassName={style.active} href="/">
+          Home
                 </Link>
-                <Link activeClassName={style.active} href="/profile">
-                    Me
+        <Link activeClassName={style.active} href="/profile">
+          Me
                 </Link>
-                <Link activeClassName={style.active} href="/profile/john">
-                    John
+        <Link activeClassName={style.active} href="/profile/john">
+          John
                 </Link>
-            </nav>
-        </header>
-    );
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
