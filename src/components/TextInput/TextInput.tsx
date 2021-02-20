687f4159d2a7dbdf9a844c3f4f1949f0c8302319
Lucas Root <lucas@simpleswap.io>
2021-02-20 16:46:42 +0300

    refactor: TextInput component prop types

--- src/components/TextInput/TextInput.tsx
+++ src/components/TextInput/TextInput.tsx
@@ -7,10 +7,13 @@ interface Props {
   label: string
   value: string
   onChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+  type?: string
+  minLength?: number
+  maxLength?: number
 }
 
 const TextInput: React.FC<Props> = (props) => {
-  const { label, value, onChange } = props
+  const { label, value, onChange, type = 'text', minLength, maxLength } = props
 
   const [isFocused, setIsFocused] = React.useState<boolean>(false)
   const textInputRef = React.useRef<HTMLInputElement>(null)
@@ -30,7 +33,15 @@ const TextInput: React.FC<Props> = (props) => {
     <Styles.Container onClick={onClick}>
       <Styles.Label>{label}</Styles.Label>
       {value || isFocused ? (
-        <Styles.TextInput value={value} onChange={onChange} ref={textInputRef} onBlur={onBlur} />
+        <Styles.TextInput
+          value={value}
+          onChange={onChange}
+          ref={textInputRef}
+          onBlur={onBlur}
+          type={type}
+          minLength={minLength}
+          maxLength={maxLength}
+        />
       ) : null}
     </Styles.Container>
   )
