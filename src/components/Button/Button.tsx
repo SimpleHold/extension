a2897b942f17ee3a169706b34c1296b672e3b978
Lucas Root <lucas@simpleswap.io>
2021-02-20 16:46:36 +0300

    refactor: Button component prop types

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -5,13 +5,14 @@ import Styles from './styles'
 
 interface Props {
   label: string
+  onClick: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void
 }
 
 const Button: React.FC<Props> = (props) => {
-  const { label } = props
+  const { label, onClick } = props
 
   return (
-    <Styles.Container>
+    <Styles.Container onClick={onClick}>
       <Styles.Label>{label}</Styles.Label>
     </Styles.Container>
   )
