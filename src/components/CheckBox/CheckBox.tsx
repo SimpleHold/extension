ff1a8d9f59a0fee779bc9497912cb8ddc0fd3d4e
lukas-ss <lucas@simpleswap.io>
2021-02-26 12:02:17 +0300

    refactor: checkbox component, add new icon

--- src/components/CheckBox/CheckBox.tsx
+++ src/components/CheckBox/CheckBox.tsx
@@ -1,16 +1,25 @@
 import * as React from 'react'
+import SVG from 'react-inlinesvg'
+
+// Icons
+import checkIcon from '@assets/icons/check.svg'
 
 // Styles
 import Styles from './styles'
 
 interface Props {
   value: boolean
+  onClick: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void
 }
 
 const CheckBox: React.FC<Props> = (props) => {
-  const { value } = props
+  const { value, onClick } = props
 
-  return <Styles.Container />
+  return (
+    <Styles.Container onClick={onClick}>
+      {value ? <SVG src={checkIcon} width={9.5} height={7.5} title="check" /> : null}
+    </Styles.Container>
+  )
 }
 
 export default CheckBox
