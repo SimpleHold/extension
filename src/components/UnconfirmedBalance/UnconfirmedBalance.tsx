fa47686d45fa7ede0310086bcfbf33f80f965047
Lucas Root <lucas@simpleswap.io>
2021-03-18 19:05:08 +0300

    refactor: UnconfirmedBalance component

--- src/components/UnconfirmedBalance/UnconfirmedBalance.tsx
+++ src/components/UnconfirmedBalance/UnconfirmedBalance.tsx
@@ -1,5 +1,8 @@
 import * as React from 'react'
 
+// Components
+import Skeleton from '@components/Skeleton'
+
 // Utils
 import { price } from '@utils/format'
 
@@ -7,24 +10,29 @@ import { price } from '@utils/format'
 import Styles from './styles'
 
 interface Props {
-  value: number
+  btcValue: number
+  type: 'light' | 'gray'
 }
 
 const UnconfirmedBalance: React.FC<Props> = (props) => {
-  const { value } = props
+  const { btcValue, type } = props
 
-  const [USDValue, setUSDValue] = React.useState<number>(0)
+  const [USDValue, setUSDValue] = React.useState<number | null>(null)
 
   React.useEffect(() => {
-    setUSDValue(value * 1000) // Fix me
-  }, [value])
+    setUSDValue(btcValue * 1000) // Fix me
+  }, [btcValue])
 
   return (
-    <Styles.Container>
-      <Styles.Title>Unconfirmed balance:</Styles.Title>
+    <Styles.Container type={type}>
+      <Styles.Title type={type}>Unconfirmed balance:</Styles.Title>
       <Styles.Values>
-        <Styles.BTCValue>{value} BTC</Styles.BTCValue>
-        <Styles.USDValue>{`$${price(USDValue)} USD`}</Styles.USDValue>
+        <Styles.BTCValue type={type}>{btcValue} BTC</Styles.BTCValue>
+        <Skeleton width={80} height={14} isLoading={USDValue === null} type={type}>
+          {USDValue ? (
+            <Styles.USDValue type={type}>{`$${price(USDValue)} USD`}</Styles.USDValue>
+          ) : null}
+        </Skeleton>
       </Styles.Values>
     </Styles.Container>
   )
--- src/components/UnconfirmedBalance/styles.ts
+++ src/components/UnconfirmedBalance/styles.ts
@@ -1,13 +1,19 @@
 import styled from 'styled-components'
 
+type TProps = {
+  type: 'light' | 'gray'
+}
+
 const Container = styled.div`
   padding: 10px 20px;
   display: flex;
   flex-direction: row;
   align-items: center;
   justify-content: space-between;
-  background: rgba(255, 255, 255, 0.1);
-  border: 1px solid rgba(255, 255, 255, 0.5);
+  background-color: ${({ type }: TProps) =>
+    type === 'light' ? 'rgba(255, 255, 255, 0.1)' : '#F8F8F8'};
+  border: ${({ type }: TProps) =>
+    `1px solid ${type === 'light' ? 'rgba(255, 255, 255, 0.5)' : '#F8F8F8'}`};
   backdrop-filter: blur(30px);
   border-radius: 5px;
 `
@@ -16,27 +22,27 @@ const Title = styled.p`
   margin: 0;
   font-size: 12px;
   line-height: 14px;
-  color: #ffffff;
-  opacity: 0.8;
+  color: ${({ type }: TProps) => (type === 'light' ? '#FFFFFF' : '#C3C3C3')};
+  opacity: ${({ type }: TProps) => (type === 'light' ? '0.8' : '1')};
 `
 
 const Values = styled.div``
 
 const BTCValue = styled.p`
-  margin: 0;
+  margin: 0 0 4px 0;
   font-weight: 500;
   font-size: 14px;
   line-height: 16px;
   text-align: right;
-  color: #ffffff;
+  color: ${({ type }: TProps) => (type === 'light' ? '#FFFFFF' : '#7D7E8D')};
 `
 
 const USDValue = styled.p`
-  margin: 4px 0 0 0;
+  margin: 0;
   font-size: 12px;
   line-height: 14px;
   text-align: right;
-  color: #ffffff;
+  color: ${({ type }: TProps) => (type === 'light' ? '#FFFFFF' : '#7D7E8D')};
 `
 
 const Styles = {
