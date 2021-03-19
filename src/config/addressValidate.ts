feb8f2f6662eda463945dce989ac7bd3ae7ff32d
Lucas Root <lucas@simpleswap.io>
2021-03-19 18:20:11 +0300

    feat: add addressValidate config file

---
+++ src/config/addressValidate.ts
@@ -0,0 +1,11 @@
+export default {
+  btc: '^[13][a-km-zA-HJ-NP-Z1-9]{25,80}$|^(bc1)[0-9A-Za-z]{25,80}$',
+  bch:
+    '^([13][a-km-zA-HJ-NP-Z1-9]{25,34})$|^((bitcoincash:)?(q|p)[a-z0-9]{41})$|^((BITCOINCASH:)?(Q|P)[A-Z0-9]{41})$',
+  bsv: '^[13][a-km-zA-HJ-NP-Z1-9]{25,34}$',
+  ltc: '^(L|M|3)[A-Za-z0-9]{33}$|^(ltc1)[0-9A-Za-z]{39}$',
+  doge: '^(D|A|9)[a-km-zA-HJ-NP-Z1-9]{33,34}$',
+  dash: '^[X|7][0-9A-Za-z]{33}$',
+  zec: '^(t)[A-Za-z0-9]{34}$',
+  grs: '^(F|3)[0-9A-za-z]{33}$',
+}
