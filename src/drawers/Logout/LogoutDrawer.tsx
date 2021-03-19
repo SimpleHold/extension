9f688c14ed0cf1411a041c75d625981fe62b2ca6
Lucas Root <lucas@simpleswap.io>
2021-03-19 15:44:57 +0300

    feat: add Logout drawer

---
+++ src/drawers/Logout/LogoutDrawer.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react'
+
+// Components
+import DrawerWrapper from '@components/DrawerWrapper'
+import Button from '@components/Button'
+
+// Styles
+import Styles from './styles'
+
+interface Props {
+  onClose: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
+  isActive: boolean
+  onConfirm: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void
+}
+
+const LogoutDrawer: React.FC<Props> = (props) => {
+  const { onClose, isActive, onConfirm } = props
+
+  return (
+    <DrawerWrapper title="Confirm log out" isActive={isActive} onClose={onClose}>
+      <Styles.Row>
+        <Styles.Text>
+          Are you sure that you want to clear cache and log out? Note: backup file will downloaded
+          after you confirm this action
+        </Styles.Text>
+
+        <Styles.Actions>
+          <Button label="Cancel" isLight isSmall onClick={onClose} mr={7.5} />
+          <Button label="Ok" isSmall onClick={onConfirm} ml={7.5} />
+        </Styles.Actions>
+      </Styles.Row>
+    </DrawerWrapper>
+  )
+}
+
+export default LogoutDrawer
---
+++ src/drawers/Logout/index.ts
@@ -0,0 +1,3 @@
+import LogoutDrawer from './LogoutDrawer'
+
+export default LogoutDrawer
