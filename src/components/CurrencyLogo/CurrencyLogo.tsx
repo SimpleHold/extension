4da716001074a0e550550e66202de8229aef2a39
Lucas Root <lucas@simpleswap.io>
2021-04-05 18:58:30 +0300

    refactor: CurrencyLogo component

--- src/components/CurrencyLogo/CurrencyLogo.tsx
+++ src/components/CurrencyLogo/CurrencyLogo.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 
 import { getCurrency } from '@config/currencies'
+import { getToken } from '@config/tokens'
 
 // Styles
 import Styles from './styles'
@@ -10,17 +11,38 @@ interface Props {
   height: number
   symbol: string
   br?: number
+  platform?: 'eth' | 'bsc'
 }
 
 const CurrencyLogo: React.FC<Props> = (props) => {
-  const { width, height, symbol, br } = props
+  const { width, height, symbol, br, platform } = props
+
+  if (platform) {
+    const token = getToken(symbol, platform)
+    const platformLogo = getCurrency(platform)
+
+    if (token) {
+      return (
+        <Styles.Container width={width} height={height} background={token.background} br={br}>
+          <Styles.LogoRow>
+            <Styles.Logo source={token.logo} width={width} height={height} />
+            {platformLogo ? (
+              <Styles.PlatformLogoRow>
+                <Styles.PlatformLogo source={platformLogo.logo} />
+              </Styles.PlatformLogoRow>
+            ) : null}
+          </Styles.LogoRow>
+        </Styles.Container>
+      )
+    }
+  }
 
   const currency = getCurrency(symbol)
 
   if (currency) {
     return (
       <Styles.Container width={width} height={height} background={currency.background} br={br}>
-        <Styles.Logo source={currency.logo} />
+        <Styles.Logo source={currency.logo} width={width} height={height} />
       </Styles.Container>
     )
   }
--- src/components/CurrencyLogo/styles.ts
+++ src/components/CurrencyLogo/styles.ts
@@ -9,33 +9,62 @@ type TContainerProps = {
 
 type TLogoProps = {
   source: string
+  width: number
+  height: number
+}
+
+type TPlatformLogoProps = {
+  source: string
 }
 
 const Container = styled.div`
   width: ${({ width }: TContainerProps) => `${width}px`};
   min-width: ${({ width }: TContainerProps) => `${width}px`};
   height: ${({ height }: TContainerProps) => `${height}px`};
-  background-color: ${({ background }: TContainerProps) => background};
-  border-radius: ${({ br }: TContainerProps) => (br ? `${br}px` : '5px')};
+  background: ${({ background }: TContainerProps) =>
+    `linear-gradient(135deg, rgba(255, 255, 255, 0.7) 0%, rgba(255, 255, 255, 0.1) 100%), ${background};`};
+  border-radius: ${({ br }: TContainerProps) => (br ? `${br}px` : '13px')};
   display: flex;
   align-items: center;
   justify-content: center;
-
-  div {
-    width: ${({ width }: TContainerProps) => `${width / 2}px`};
-    height: ${({ height }: TContainerProps) => `${height / 2}px`};
-  }
 `
 
 const Logo = styled.div<TLogoProps>`
   background-image: ${({ source }: TLogoProps) => `url(${source})`};
   background-size: contain;
   background-repeat: no-repeat;
+  width: ${({ width }: TLogoProps) => `${width / 2}px`};
+  height: ${({ height }: TLogoProps) => `${height / 2}px`};
+`
+
+const LogoRow = styled.div``
+
+const PlatformLogoRow = styled.div`
+  width: 18px;
+  height: 18px;
+  background: linear-gradient(135deg, rgba(255, 255, 255, 0.5) 0%, rgba(255, 255, 255, 0) 100%),
+    #1d1d22;
+  border: 1px solid #ffffff;
+  border-radius: 13px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const PlatformLogo = styled.div`
+  background-image: ${({ source }: TPlatformLogoProps) => `url(${source})`};
+  background-size: contain;
+  background-repeat: no-repeat;
+  width: 10px;
+  height: 10px;
 `
 
 const Styles = {
   Container,
   Logo,
+  LogoRow,
+  PlatformLogoRow,
+  PlatformLogo,
 }
 
 export default Styles
