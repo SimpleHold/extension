6bfb6d7f6d9fc06d3d6bbb4281f2dcfd3ee963a6
Lucas Root <lucas@simpleswap.io>
2021-04-19 18:44:39 +0300

    feat: update extension version

--- src/utils/backup.ts
+++ src/utils/backup.ts
@@ -14,7 +14,7 @@ export const generate = (address: string, privateKey: string): { [key: string]: 
         privateKey,
       },
     ],
-    version: 1.1,
+    version: 1.11,
     uuid: v4(),
   })
 
