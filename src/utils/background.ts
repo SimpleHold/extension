2cc53b706e796e8d7341dc4680320eb89adf5abe
Lucas Root <lucas@simpleswap.io>
2021-04-29 19:19:35 +0300

    refactor: background and contentScript files

--- src/utils/background.ts
+++ src/utils/background.ts
@@ -1,6 +1,33 @@
-import 'emoji-log'
 import { browser } from 'webextension-polyfill-ts'
 
-browser.runtime.onInstalled.addListener((): void => {
-  console.emoji('🦄', 'extension installed')
+export interface IRequest {
+  type: string
+  data?: any
+}
+
+browser.runtime.onMessage.addListener(async (request: IRequest) => {
+  if (request.type === 'request_addresses') {
+    const { data: { inputId = undefined } = {} } = request
+
+    if (inputId) {
+      const { screenX, outerWidth } = window
+
+      browser.windows.create({
+        url: 'select-address.html',
+        type: 'popup',
+        width: 375,
+        height: 700,
+        left: Math.max(screenX + (outerWidth - 375), 0),
+        top: 0,
+      })
+    }
+  }
+
+  if (request.type === 'set_address') {
+    const tabs = await browser.tabs.query({ active: true })
+
+    if (tabs[0]?.id) {
+      browser.tabs.sendMessage(tabs[0].id, request)
+    }
+  }
 })
--- src/utils/contentScript.ts
+++ src/utils/contentScript.ts
@@ -1,3 +1,27 @@
-console.log('helloworld from content script')
+import { browser } from 'webextension-polyfill-ts'
 
-export {}
+export interface IRequest {
+  type: string
+  data?: any
+}
+
+document.getElementById('simpleHoldButton')?.addEventListener('click', async () => {
+  browser.runtime.sendMessage({
+    type: 'request_addresses',
+    data: {
+      inputId: 'myinput',
+    },
+  })
+})
+
+browser.runtime.onMessage.addListener((request: IRequest) => {
+  if (request.type === 'set_address') {
+    const { data } = request
+
+    const input = document.getElementById('myinput') as HTMLInputElement
+
+    if (input && data?.address) {
+      input.value = data.address
+    }
+  }
+})
