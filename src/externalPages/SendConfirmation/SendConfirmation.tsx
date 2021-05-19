61fc33cf72e7504ebd6518ed9af15daed90c756f
Lucas Root <lucas@simpleswap.io>
2021-05-19 18:40:40 +0300

    refactor: SendConfirmation page

--- src/externalPages/SendConfirmation/SendConfirmation.tsx
+++ src/externalPages/SendConfirmation/SendConfirmation.tsx
@@ -4,13 +4,73 @@ import { render } from 'react-dom'
 // Container
 import ExternalPageContainer from '@containers/ExternalPage'
 
+// Components
+import Button from '@components/Button'
+
+// Drawers
+import ConfirmDrawer from '@drawers/Confirm'
+
+// Utils
+import { toUpper } from '@utils/format'
+import { validatePassword } from '@utils/validate'
+
 // Styles
 import Styles from './styles'
 
+interface Props {
+  amount?: number
+  symbol?: string
+  addressFrom?: string
+  addressTo?: string
+  networkFee?: number
+  tabInfo?: {
+    favIconUrl: string
+    url: string
+  }
+}
+
 const SendConfirmation: React.FC = () => {
+  const [props, setProps] = React.useState<Props>({})
+  const [activeDrawer, setActiveDrawer] = React.useState<'confirm' | null>(null)
+  const [password, setPassword] = React.useState<string>('')
+  const [passwordErrorLabel, setPasswordErrorLabel] = React.useState<null | string>(null)
+
+  React.useEffect(() => {
+    checkProps()
+  }, [])
+
+  const parseJson = (value: string): { [key: string]: any } | null => {
+    try {
+      return JSON.parse(value)
+    } catch {
+      return null
+    }
+  }
+
+  const checkProps = async (): Promise<void> => {
+    const data = localStorage.getItem('sendConfirmationData')
+
+    if (data) {
+      const parseData = parseJson(data)
+
+      if (parseData) {
+        localStorage.removeItem('sendConfirmationData')
+        return setProps(parseData)
+      }
+    }
+
+    onClose()
+  }
+
   const onClose = (): void => {
     window.close()
   }
+
+  const onConfirm = (): void => {
+    setActiveDrawer('confirm')
+  }
+
+  const onConfirmSend = () => {}
 
   return (
     <ExternalPageContainer
@@ -19,9 +79,79 @@ const SendConfirmation: React.FC = () => {
       backPageTitle="Send"
       backPageUrl="send.html"
     >
-      <Styles.Body>
-        <p>SendConfirmation</p>
-      </Styles.Body>
+      <>
+        <Styles.Body>
+          <Styles.Row>
+            <Styles.Title>Confirm the sending</Styles.Title>
+            <Styles.SiteInfo>
+              <Styles.SiteInfoLabel>Confirm sending on</Styles.SiteInfoLabel>
+              {props?.tabInfo ? (
+                <Styles.SiteInfoRow>
+                  <Styles.SiteFavicon src={props.tabInfo.favIconUrl} />
+                  <Styles.SiteUrl>{props.tabInfo.url}</Styles.SiteUrl>
+                </Styles.SiteInfoRow>
+              ) : null}
+            </Styles.SiteInfo>
+
+            <Styles.OrderCheck>
+              <Styles.List>
+                <Styles.ListTitle>Amount:</Styles.ListTitle>
+                <Styles.ListRow>
+                  <Styles.Amount>{props?.amount}</Styles.Amount>
+                  <Styles.ListText>{toUpper(props.symbol)}</Styles.ListText>
+                </Styles.ListRow>
+              </Styles.List>
+              <Styles.List>
+                <Styles.ListTitle>Network fee:</Styles.ListTitle>
+                <Styles.ListRow>
+                  <Styles.Amount>{props.networkFee}</Styles.Amount>
+                  <Styles.ListText>{toUpper(props.symbol)}</Styles.ListText>
+                </Styles.ListRow>
+              </Styles.List>
+
+              <Styles.DashedDivider>
+                <Styles.DashedDividerLine />
+              </Styles.DashedDivider>
+
+              <Styles.List>
+                <Styles.ListTitle>Total:</Styles.ListTitle>
+                <Styles.ListRow>
+                  <Styles.Amount>{Number(props?.amount) + Number(props.networkFee)}</Styles.Amount>
+                  <Styles.ListText>{toUpper(props.symbol)}</Styles.ListText>
+                </Styles.ListRow>
+              </Styles.List>
+            </Styles.OrderCheck>
+
+            <Styles.DestinationsList>
+              <Styles.Destinate>
+                <Styles.DestinateTitle>From</Styles.DestinateTitle>
+                <Styles.DestinateText>bc1q34aq5drpuywhup9892qp6svr8ldz</Styles.DestinateText>
+              </Styles.Destinate>
+              <Styles.Destinate>
+                <Styles.DestinateTitle>To</Styles.DestinateTitle>
+                <Styles.DestinateText>bc1q34aq5rpwy3whup9892qp6svr8ldz</Styles.DestinateText>
+              </Styles.Destinate>
+            </Styles.DestinationsList>
+          </Styles.Row>
+          <Styles.Actions>
+            <Button label="Cancel" isSmall isLight onClick={onClose} mr={7.5} />
+            <Button label="Confirm" isSmall onClick={onConfirm} ml={7.5} />
+          </Styles.Actions>
+        </Styles.Body>
+        <ConfirmDrawer
+          isActive={activeDrawer === 'confirm'}
+          onClose={() => setActiveDrawer(null)}
+          title="Enter the password to restore your wallet"
+          textInputValue={password}
+          onChangeText={setPassword}
+          onConfirm={onConfirmSend}
+          textInputType="password"
+          inputLabel="Enter password"
+          isButtonDisabled={!validatePassword(password)}
+          inputErrorLabel={passwordErrorLabel}
+          openFrom="browser"
+        />
+      </>
     </ExternalPageContainer>
   )
 }
--- src/externalPages/SendConfirmation/styles.ts
+++ src/externalPages/SendConfirmation/styles.ts
@@ -7,8 +7,195 @@ const Body = styled.div`
   flex-direction: column;
 `
 
+const Row = styled.div`
+  padding: 40px 30px 0 30px;
+  flex: 1;
+`
+
+const Title = styled.p`
+  margin: 0;
+  font-weight: bold;
+  font-size: 30px;
+  line-height: 35px;
+  color: #1d1d22;
+`
+
+const SiteInfo = styled.div`
+  margin: 10px 0 0 0;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`
+
+const SiteInfoLabel = styled.p`
+  margin: 0;
+  font-size: 16px;
+  line-height: 19px;
+  color: #7d7e8d;
+`
+
+const SiteInfoRow = styled.div`
+  margin: 0 0 0 10px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`
+
+const SiteFavicon = styled.img`
+  width: 16px;
+  height: 16px;
+  border-radius: 8px;
+  margin: 0 5px 0 0;
+`
+
+const SiteUrl = styled.p`
+  margin: 0;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 19px;
+  color: #7d7e8d;
+`
+
+const OrderCheck = styled.div`
+  margin: 50px 0 0 0;
+`
+
+const List = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  margin: 0 0 10px 0;
+`
+
+const ListTitle = styled.p`
+  margin: 0;
+  font-size: 16px;
+  line-height: 19px;
+  color: #7d7e8d;
+`
+
+const ListText = styled.p`
+  margin: 0;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 19px;
+  color: #1d1d22;
+  width: 40px;
+`
+
+const DashedDivider = styled.div`
+  padding: 5px 0 15px 0;
+`
+
+const DashedDividerLine = styled.div`
+  width: 100%;
+  border-top: 1px dashed #c3c3c3;
+`
+
+const ListRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`
+
+const Amount = styled.p`
+  margin: 0;
+  width: 90px;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 19px;
+  color: #1d1d22;
+`
+
+const DestinationsList = styled.ul`
+  list-style-type: none;
+  padding: 30px 0 0 20px;
+  overflow: hidden;
+`
+
+const Destinate = styled.li`
+  position: relative;
+  margin-bottom: 0;
+  padding-bottom: 20px;
+
+  &:before {
+    content: '';
+    position: absolute;
+    left: -17px;
+    border-left: 1px solid #c3c3c3;
+    height: 100%;
+    width: 1px;
+  }
+
+  &:after {
+    content: '';
+    width: 7px;
+    height: 7px;
+    background-color: #c3c3c3;
+    border-radius: 4px;
+    position: absolute;
+    left: -20px;
+    top: 5px;
+  }
+
+  &:first-child:before {
+    top: 6px;
+  }
+
+  &:last-child:before {
+    height: 6px;
+  }
+`
+
+const DestinateTitle = styled.p`
+  margin: 0;
+  font-size: 14px;
+  line-height: 16px;
+  text-transform: uppercase;
+  color: #7d7e8d;
+`
+
+const DestinateText = styled.p`
+  margin: 5px 0 0 0;
+  font-weight: normal;
+  font-size: 16px;
+  line-height: 19px;
+  color: #1d1d22;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+`
+
+const Actions = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  padding: 0 30px 30px 30px;
+`
+
 const Styles = {
   Body,
+  Row,
+  Title,
+  SiteInfo,
+  SiteInfoLabel,
+  SiteInfoRow,
+  SiteFavicon,
+  SiteUrl,
+  OrderCheck,
+  List,
+  ListTitle,
+  ListText,
+  DashedDivider,
+  DashedDividerLine,
+  ListRow,
+  Amount,
+  DestinationsList,
+  Destinate,
+  DestinateTitle,
+  DestinateText,
+  Actions,
 }
 
 export default Styles
