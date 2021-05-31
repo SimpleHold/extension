6e813905ea4b7ce15d5c3ba8fb3ee7d2209792b3
Lucas Root <lucas@simpleswap.io>
2021-05-31 15:51:40 +0300

    feat: add storage util

---
+++ src/utils/storage.ts
@@ -0,0 +1,17 @@
+export const getItem = (key: string): string | null => {
+  return localStorage.getItem(key)
+}
+
+export const setItem = (key: string, value: string): void => {
+  return localStorage.setItem(key, value)
+}
+
+export const removeItem = (key: string): void => {
+  return localStorage.removeItem(key)
+}
+
+export const removeMany = (keys: string[]): void => {
+  for (const key of keys) {
+    removeItem(key)
+  }
+}
