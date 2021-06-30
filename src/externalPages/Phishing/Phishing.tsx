d7f7672a34f5301bb894e8693d2ecb9e17ac77a8
Lucas Root <lucas@simpleswap.io>
2021-06-30 18:58:50 +0300

    refactor: Phishing external page

--- src/externalPages/Phishing/Phishing.tsx
+++ src/externalPages/Phishing/Phishing.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react'
 import { render } from 'react-dom'
 
+// Components
+import CheckBox from '@components/CheckBox'
+
 // Utils
 import { removeItem, getItem } from '@utils/storage'
 import { getUrl, getTabs, removeTabs } from '@utils/extension'
@@ -10,9 +13,20 @@ import { openWebPage } from '@utils/extension'
 import Styles from './styles'
 
 const Phishing: React.FC = () => {
+  const [isAgreedVisible, setAgreedVisible] = React.useState<boolean>(false)
+  const [isAgreed, setIsAgreed] = React.useState<boolean>(false)
+  const [rightUrl, setRightUrl] = React.useState<string | null>(null)
+  const [rightSiteName, setRightSiteName] = React.useState<string | null>(null)
+
   React.useEffect(() => {
     closePhishingSite()
+    getRightSite()
   }, [])
+
+  const getRightSite = (): void => {
+    setRightUrl('https://www.myetherwallet.com/')
+    setRightSiteName('SimpleSwap')
+  }
 
   const closePhishingSite = async (): Promise<void> => {
     const urls = localStorage.getItem('phishingUrls')
@@ -34,9 +48,7 @@ const Phishing: React.FC = () => {
     removeItem('latestPhishingSite')
   }
 
-  const onVisit = async (): Promise<void> => {
-    const siteUrl = getItem('phishingSite')
-
+  const onVisit = async (siteUrl: null | string): Promise<void> => {
     if (siteUrl) {
       const tab = await getTabs({
         active: true,
@@ -51,6 +63,30 @@ const Phishing: React.FC = () => {
     }
   }
 
+  const onClickButton = (): void => {
+    if (!isAgreedVisible) {
+      setAgreedVisible(true)
+    } else {
+      if (isAgreed) {
+        const siteUrl = getItem('phishingSite')
+
+        onVisit(siteUrl)
+      }
+    }
+  }
+
+  const toggleAgreed = (e: React.MouseEvent<HTMLDivElement, MouseEvent>): void => {
+    e.stopPropagation()
+
+    setIsAgreed((prevValue: boolean) => !prevValue)
+  }
+
+  const onVisitRightSite = (): void => {
+    if (rightUrl) {
+      onVisit(rightUrl)
+    }
+  }
+
   return (
     <Styles.Wrapper>
       <Styles.Logo />
@@ -59,16 +95,48 @@ const Phishing: React.FC = () => {
         <Styles.WarningRow>
           <Styles.Image />
           <Styles.Title>Be careful</Styles.Title>
-          <Styles.Description>
-            It looks like the website you're going to visit is not what it seems to be. This page is
-            marked as a phishing scam by our partners SimpleSwap. Tap this button to find the
-            original SimpleHold you're looking for.
-          </Styles.Description>
+          {rightSiteName ? (
+            <Styles.Description>
+              It looks like the website you're going to visit is not what it seems to be. This page
+              is marked as a phishing scam by our partners {rightSiteName}. Tap this button to find
+              the original SimpleHold you're looking for.
+            </Styles.Description>
+          ) : null}
+
+          {rightUrl ? (
+            <Styles.RightSiteBlock onClick={onVisitRightSite}>
+              <Styles.RightSiteIconRow>
+                <Styles.RightBlockIcon />
+              </Styles.RightSiteIconRow>
+              <Styles.RightSiteBlockRow>
+                <Styles.RightSiteBlockTitle>Go to right site:</Styles.RightSiteBlockTitle>
+                <Styles.RightSiteUrl>{rightUrl}</Styles.RightSiteUrl>
+              </Styles.RightSiteBlockRow>
+            </Styles.RightSiteBlock>
+          ) : null}
         </Styles.WarningRow>
         <Styles.WarningFooter>
-          <Styles.AdvancedButton>
-            <Styles.AdvancedButtonTitle>Advanced</Styles.AdvancedButtonTitle>
-          </Styles.AdvancedButton>
+          <Styles.AdvancedRow>
+            <Styles.AgreedBlock isVisible={isAgreedVisible} onClick={toggleAgreed}>
+              {isAgreedVisible ? (
+                <>
+                  <CheckBox value={isAgreed} onClick={toggleAgreed} color="#31A76C" />
+                  <Styles.AgreedText>
+                    I understand all risks about phishing sites and want to go on
+                  </Styles.AgreedText>
+                </>
+              ) : null}
+            </Styles.AgreedBlock>
+            <Styles.AdvancedButton
+              isVisible={isAgreedVisible}
+              isAgreed={isAgreed}
+              onClick={onClickButton}
+            >
+              <Styles.AdvancedButtonTitle>
+                {isAgreedVisible ? 'Continue' : 'Advanced'}
+              </Styles.AdvancedButtonTitle>
+            </Styles.AdvancedButton>
+          </Styles.AdvancedRow>
         </Styles.WarningFooter>
       </Styles.Warning>
     </Styles.Wrapper>
--- src/externalPages/Phishing/styles.ts
+++ src/externalPages/Phishing/styles.ts
@@ -1,5 +1,18 @@
 import styled from 'styled-components'
 
+import logo from '@assets/fullLogo.svg'
+import illustrate from '@assets/illustrate/phishing.svg'
+import shieldIcons from '@assets/icons/shield.svg'
+
+type TAgreedBlockProps = {
+  isVisible: boolean
+}
+
+type TAdvancedButtonProps = {
+  isVisible: boolean
+  isAgreed: boolean
+}
+
 const Wrapper = styled.div`
   padding: 40px 0 0 0;
   display: flex;
@@ -10,7 +23,9 @@ const Wrapper = styled.div`
 const Logo = styled.div`
   width: 161px;
   height: 24px;
-  background-color: red;
+  background-image: url(${logo});
+  background-repeat: no-repeat;
+  background-size: contain;
 `
 
 const Warning = styled.div`
@@ -30,7 +45,9 @@ const WarningRow = styled.div`
 const Image = styled.div`
   width: 384px;
   height: 130px;
-  background-color: red; ;
+  background-image: url(${illustrate});
+  background-repeat: no-repeat;
+  background-size: contain;
 `
 
 const Title = styled.h1`
@@ -53,13 +70,130 @@ const Description = styled.p`
 const WarningFooter = styled.div`
   padding: 20px 40px;
   border-top: 1px solid #ffffff;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: flex-start;
 `
 
 const AdvancedButton = styled.div`
   padding: 12px 20px;
+  background-color: ${({ isVisible }: TAdvancedButtonProps) =>
+    isVisible ? '#3FBB7D' : 'rgba(166, 217, 188, 0.5)'};
+  border-radius: ${({ isVisible }: TAdvancedButtonProps) => (isVisible ? '0 5px 5px 0' : '5px')};
+  transition: all 0.3s;
+  opacity: ${({ isVisible, isAgreed }: TAdvancedButtonProps) =>
+    isVisible && !isAgreed ? '0.5' : '1'};
+
+  p {
+    color: ${({ isVisible }: TAdvancedButtonProps) => (isVisible ? '#ffffff' : '#3fbb7d')};
+  }
+
+  &:hover {
+    cursor: ${({ isVisible, isAgreed }: TAdvancedButtonProps) =>
+      isVisible ? (isAgreed ? 'pointer' : 'default') : 'pointer'};
+    background-color: ${({ isAgreed, isVisible }: TAdvancedButtonProps) =>
+      isAgreed ? '#31A76C' : isVisible ? '#3FBB7D' : '#ffffff'};
+  }
 `
 
-const AdvancedButtonTitle = styled.p``
+const AdvancedButtonTitle = styled.p`
+  margin: 0;
+  font-weight: bold;
+  font-size: 14px;
+  line-height: 16px;
+`
+
+const RightSiteBlock = styled.div`
+  padding: 15px;
+  background-color: #3fbb7d;
+  border-radius: 16px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin: 30px 0 0 0;
+
+  &:hover {
+    cursor: pointer;
+
+    p {
+      text-decoration: underline;
+    }
+  }
+`
+
+const RightSiteIconRow = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 20px;
+  background-color: #ffffff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const RightSiteBlockRow = styled.div`
+  margin: 0 0 0 10px;
+`
+
+const RightSiteBlockTitle = styled.span`
+  font-size: 14px;
+  line-height: 16px;
+  color: #ffffff;
+`
+
+const RightSiteUrl = styled.p`
+  margin: 5px 0 0 0;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 19px;
+  color: #ffffff;
+`
+
+const RightBlockIcon = styled.div`
+  width: 24px;
+  height: 24px;
+  background-image: url(${shieldIcons});
+  background-repeat: no-repeat;
+  background-size: contain;
+`
+
+const AdvancedRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`
+
+const AgreedBlock = styled.div`
+  background-color: #ffffff;
+  transition: width 0.2s ease-in-out;
+  width: ${({ isVisible }: TAgreedBlockProps) => (isVisible ? '440px' : '0px')};
+  border-radius: 5px 0 0 5px;
+  margin: 0 1px 0 0;
+  padding: ${({ isVisible }: TAgreedBlockProps) => (isVisible ? '10.5px 20px' : '0')};
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  &:hover {
+    cursor: pointer;
+
+    p {
+      color: #31a76c;
+    }
+  }
+`
+
+const AgreedText = styled.p`
+  margin: 0 0 0 10px;
+  font-size: 14px;
+  line-height: 16px;
+  color: #3fbb7d;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  overflow: hidden;
+  flex: 1;
+`
 
 const Styles = {
   Wrapper,
@@ -72,6 +206,15 @@ const Styles = {
   WarningFooter,
   AdvancedButton,
   AdvancedButtonTitle,
+  RightSiteBlock,
+  RightSiteIconRow,
+  RightSiteBlockRow,
+  RightSiteBlockTitle,
+  RightSiteUrl,
+  RightBlockIcon,
+  AdvancedRow,
+  AgreedBlock,
+  AgreedText,
 }
 
 export default Styles
