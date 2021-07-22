7f3491c7ad673f498fbd02f3d0f5022fef691652
Lucas Root <lucas@simpleswap.io>
2021-07-22 18:14:48 +0300

    feat: add new Receive page

--- src/pages/Receive/styles.ts
+++ src/pages/Receive/styles.ts
@@ -1,167 +1,88 @@
-import styled, { keyframes } from 'styled-components'
-
-const rotating = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
-`
-
-type TRefreshButtonProps = {
-  isRefreshing: boolean
-}
+import styled from 'styled-components'
 
 const Wrapper = styled.div`
   height: 600px;
-  overflow: hidden;
 `
 
 const Container = styled.div`
-  background-color: #ffffff;
-  border-radius: 5px 5px 0 0;
+  background: #f2f4f8;
+  border-radius: 5px 5px 0px 0px;
   height: 540px;
-  overflow: hidden;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
+  padding: 0 30px;
 `
 
 const Row = styled.div`
-  padding: 20px 30px 35px 30px;
-  position: relative;
+  padding: 20px 0 30px 0;
 `
 
 const Heading = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
+  justify-content: space-between;
 `
 
-const Currency = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  flex: 1;
-`
-
-const CurrencyName = styled.p`
-  margin: 0 0 0 10px;
-  font-weight: 500;
+const Title = styled.p`
+  margin: 0;
+  font-weight: bold;
   font-size: 20px;
   line-height: 23px;
   color: #1d1d22;
 `
 
-const Actions = styled.div`
+const Receive = styled.div`
+  width: 100%;
+  height: 300px;
+  background-color: #ffffff;
+  border-radius: 16px;
+  padding: 30px 38px 0 38px;
+  margin: 20px 0 0 0;
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   align-items: center;
-
-  > div {
-    &:not(:last-child) {
-      margin: 0 5px 0 0;
-    }
-  }
+  word-break: break-all;
 `
 
-const Action = styled.div`
+const Address = styled.p`
+  margin: 16px 0 0 0;
+  font-size: 16px;
+  line-height: 23px;
+  text-align: center;
+  color: #1d1d22;
+`
+
+const Warning = styled.p`
+  margin: 20px 0 0 0;
+  font-size: 14px;
+  line-height: 20px;
+  text-align: center;
+  color: #7d7e8d;
+`
+
+const Button = styled.div`
   width: 30px;
   height: 30px;
-  background: rgba(234, 234, 234, 0.5);
-  border-radius: 5px;
+`
+
+const MoreButton = styled(Button)`
+  background: rgba(222, 225, 233, 0.6);
+  border-radius: 15px;
   display: flex;
   align-items: center;
   justify-content: center;
 
   path {
-    fill: #c3c3c3;
+    fill: #7d7e8d;
   }
 
   &:hover {
     cursor: pointer;
+    background: rgba(222, 225, 233, 0.4);
 
     path {
       fill: #3fbb7d;
     }
   }
-`
-
-const ReceiveBlock = styled.div`
-  border-top: 1px solid #eaeaea;
-  padding: 20px 30px 30px 30px;
-  background-color: #f8f8f8;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: flex-end;
-  height: 100%;
-`
-
-const Address = styled.p`
-  margin: 10px 0 0 0;
-  font-size: 16px;
-  line-height: 23px;
-  text-align: center;
-  color: #7d7e8d;
-  word-break: break-all;
-`
-
-const BalanceRow = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  margin: 30px 0 0 0;
-`
-
-const Balance = styled.p`
-  margin: 0;
-  font-weight: 500;
-  font-size: 32px;
-  line-height: 32px;
-  white-space: nowrap;
-  color: #1d1d22;
-`
-
-const RefreshButton = styled.div`
-  width: 30px;
-  height: 30px;
-  background: rgba(234, 234, 234, 0.5);
-  border-radius: 15px;
-  margin: 0 0 0 10px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
-  path {
-    fill: ${({ isRefreshing }: TRefreshButtonProps) => (isRefreshing ? '#3fbb7d' : '#c3c3c3')};
-  }
-
-  svg {
-    animation: ${rotating} 2s infinite linear;
-    animation-duration: ${({ isRefreshing }: TRefreshButtonProps) =>
-      isRefreshing ? '2s' : 'inherit'};
-  }
-
-  &:hover {
-    cursor: ${({ isRefreshing }: TRefreshButtonProps) => (isRefreshing ? 'default' : 'pointer')};
-
-    path {
-      fill: #3fbb7d;
-    }
-  }
-`
-
-const Estimated = styled.p`
-  margin: 10px 0 0 0;
-  font-size: 20px;
-  line-height: 23px;
-  color: #7d7e8d;
-`
-
-const PendingRow = styled.div`
-  margin: 20px 0 0 0;
 `
 
 const Styles = {
@@ -169,17 +90,12 @@ const Styles = {
   Container,
   Row,
   Heading,
-  Currency,
-  CurrencyName,
-  Actions,
-  Action,
-  BalanceRow,
-  Balance,
-  RefreshButton,
-  Estimated,
-  PendingRow,
-  ReceiveBlock,
+  Title,
+  Receive,
   Address,
+  Warning,
+  Button,
+  MoreButton,
 }
 
 export default Styles
---
+++ src/pages/Receive_old/styles.ts
@@ -0,0 +1,185 @@
+import styled, { keyframes } from 'styled-components'
+
+const rotating = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`
+
+type TRefreshButtonProps = {
+  isRefreshing: boolean
+}
+
+const Wrapper = styled.div`
+  height: 600px;
+  overflow: hidden;
+`
+
+const Container = styled.div`
+  background-color: #ffffff;
+  border-radius: 5px 5px 0 0;
+  height: 540px;
+  overflow: hidden;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`
+
+const Row = styled.div`
+  padding: 20px 30px 35px 30px;
+  position: relative;
+`
+
+const Heading = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`
+
+const Currency = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  flex: 1;
+`
+
+const CurrencyName = styled.p`
+  margin: 0 0 0 10px;
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 23px;
+  color: #1d1d22;
+`
+
+const Actions = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  > div {
+    &:not(:last-child) {
+      margin: 0 5px 0 0;
+    }
+  }
+`
+
+const Action = styled.div`
+  width: 30px;
+  height: 30px;
+  background: rgba(234, 234, 234, 0.5);
+  border-radius: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  path {
+    fill: #c3c3c3;
+  }
+
+  &:hover {
+    cursor: pointer;
+
+    path {
+      fill: #3fbb7d;
+    }
+  }
+`
+
+const ReceiveBlock = styled.div`
+  border-top: 1px solid #eaeaea;
+  padding: 20px 30px 30px 30px;
+  background-color: #f8f8f8;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-end;
+  height: 100%;
+`
+
+const Address = styled.p`
+  margin: 10px 0 0 0;
+  font-size: 16px;
+  line-height: 23px;
+  text-align: center;
+  color: #7d7e8d;
+  word-break: break-all;
+`
+
+const BalanceRow = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin: 30px 0 0 0;
+`
+
+const Balance = styled.p`
+  margin: 0;
+  font-weight: 500;
+  font-size: 32px;
+  line-height: 32px;
+  white-space: nowrap;
+  color: #1d1d22;
+`
+
+const RefreshButton = styled.div`
+  width: 30px;
+  height: 30px;
+  background: rgba(234, 234, 234, 0.5);
+  border-radius: 15px;
+  margin: 0 0 0 10px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  path {
+    fill: ${({ isRefreshing }: TRefreshButtonProps) => (isRefreshing ? '#3fbb7d' : '#c3c3c3')};
+  }
+
+  svg {
+    animation: ${rotating} 2s infinite linear;
+    animation-duration: ${({ isRefreshing }: TRefreshButtonProps) =>
+      isRefreshing ? '2s' : 'inherit'};
+  }
+
+  &:hover {
+    cursor: ${({ isRefreshing }: TRefreshButtonProps) => (isRefreshing ? 'default' : 'pointer')};
+
+    path {
+      fill: #3fbb7d;
+    }
+  }
+`
+
+const Estimated = styled.p`
+  margin: 10px 0 0 0;
+  font-size: 20px;
+  line-height: 23px;
+  color: #7d7e8d;
+`
+
+const PendingRow = styled.div`
+  margin: 20px 0 0 0;
+`
+
+const Styles = {
+  Wrapper,
+  Container,
+  Row,
+  Heading,
+  Currency,
+  CurrencyName,
+  Actions,
+  Action,
+  BalanceRow,
+  Balance,
+  RefreshButton,
+  Estimated,
+  PendingRow,
+  ReceiveBlock,
+  Address,
+}
+
+export default Styles
