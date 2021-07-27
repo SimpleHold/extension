7c3fa1d79784527351f7eea74a10a320129dbb78
Lucas Root <lucas@simpleswap.io>
2021-07-27 14:32:00 +0300

    feat: add new Send page

---
+++ src/pages/Send/components/SendForm/styles.tsx
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+  background-color: #ffffff;
+  border-radius: 16px;
+  padding: 50px;
+`
+
+const Styles = {
+  Container,
+}
+
+export default Styles
