88a8128912f3e55f89043d2eb432415dc97ed273
Lucas Root <lucas@simpleswap.io>
2021-07-28 16:56:49 +0300

    refactor: Receive page

--- src/pages/Receive/Receive.tsx
+++ src/pages/Receive/Receive.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import { useHistory, useLocation } from 'react-router-dom'
-import SVG from 'react-inlinesvg'
 import copy from 'copy-to-clipboard'
 
 // Components
@@ -8,7 +7,6 @@ import Cover from '@components/Cover'
 import Header from '@components/Header'
 import QRCode from '@components/QRCode'
 import Button from '@components/Button'
-import DropDown from '@components/DropDown'
 
 // Drawers
 import ExtraIdDrawer from '@drawers/ExtraId'
@@ -18,20 +16,10 @@ import { toUpper } from '@utils/format'
 import { getExtraIdName } from '@utils/address'
 import { logEvent } from '@utils/amplitude'
 
-// Hooks
-import useVisible from '@hooks/useVisible'
-
 // Config
 import { ADDRESS_RECEIVE } from '@config/events'
 import { getCurrency } from '@config/currencies'
 import { getToken } from '@config/tokens'
-
-// Assets
-import moreIcon from '@assets/icons/more.svg'
-import plusCircleIcon from '@assets/icons/plusCircle.svg'
-
-// Types
-import { TDropdowbList } from '@components/DropDown/DropDown'
 
 // Styles
 import Styles from './styles'
@@ -49,11 +37,9 @@ const ReceivePage: React.FC = () => {
   const history = useHistory()
 
   const [isCopied, setIsCopied] = React.useState<boolean>(false)
-  const [dropdownList, setDropdownList] = React.useState<TDropdowbList[]>([])
   const [extraIdName, setExtraIdName] = React.useState<string>('')
   const [activeDrawer, setActiveDrawer] = React.useState<null | 'extraId'>(null)
 
-  const { ref, isVisible, setIsVisible } = useVisible(false)
   const currency = chain ? getToken(symbol, chain) : getCurrency(symbol)
 
   React.useEffect(() => {
@@ -69,37 +55,12 @@ const ReceivePage: React.FC = () => {
       name: ADDRESS_RECEIVE,
     })
 
-    getDropdownList()
-  }, [])
-
-  const getDropdownList = (): void => {
-    const list: TDropdowbList[] = []
-
     const name = getExtraIdName(symbol)
 
     if (name) {
       setExtraIdName(name)
-
-      list.push({
-        icon: { source: plusCircleIcon, width: 18, height: 18 },
-        title: `Generate ${name}`,
-      })
     }
-
-    setDropdownList(list)
-  }
-
-  const toggleDropdown = (): void => {
-    setIsVisible(!isVisible)
-  }
-
-  const onClickDropDown = (index: number): void => {
-    toggleDropdown()
-
-    if (index === 0 && extraIdName.length) {
-      setActiveDrawer('extraId')
-    }
-  }
+  }, [])
 
   const onCopyAddress = (): void => {
     copy(address)
@@ -110,6 +71,15 @@ const ReceivePage: React.FC = () => {
     setActiveDrawer(null)
   }
 
+  const onGenerateExtraId = (): void => {
+    const name = getExtraIdName(symbol)
+
+    if (name) {
+      setExtraIdName(name)
+      setActiveDrawer('extraId')
+    }
+  }
+
   return (
     <>
       <Styles.Wrapper>
@@ -117,21 +87,7 @@ const ReceivePage: React.FC = () => {
         <Header withBack onBack={history.goBack} backTitle="Wallet" />
         <Styles.Container>
           <Styles.Row>
-            <Styles.Heading>
-              <Styles.Title>Receive {toUpper(symbol)}</Styles.Title>
-              {dropdownList.length ? (
-                <Styles.MoreButton onClick={toggleDropdown}>
-                  <SVG src={moreIcon} width={16} height={3.36} />
-                </Styles.MoreButton>
-              ) : null}
-
-              <DropDown
-                dropDownRef={ref}
-                isVisible={isVisible}
-                list={dropdownList}
-                onClick={onClickDropDown}
-              />
-            </Styles.Heading>
+            <Styles.Title>Receive {toUpper(symbol)}</Styles.Title>
 
             <Styles.Receive>
               <QRCode size={170} value={address} />
@@ -142,6 +98,12 @@ const ReceivePage: React.FC = () => {
               Send only {currency?.name} to this address. Sending any other coins may result in
               permanent loss.
             </Styles.Warning>
+
+            {extraIdName?.length ? (
+              <Styles.GenerateExtraId onClick={onGenerateExtraId}>
+                Generate {extraIdName}
+              </Styles.GenerateExtraId>
+            ) : null}
           </Styles.Row>
           <Button label={isCopied ? 'Copied!' : 'Copy address'} onClick={onCopyAddress} />
         </Styles.Container>
--- src/pages/Receive/styles.ts
+++ src/pages/Receive/styles.ts
@@ -1,9 +1,5 @@
 import styled from 'styled-components'
 
-type TCurrencyNameProps = {
-  size: 'small' | 'normal'
-}
-
 const Wrapper = styled.div`
   height: 600px;
   overflow: hidden;
@@ -13,22 +9,16 @@ const Container = styled.div`
   background-color: #f2f4f8;
   border-radius: 5px 5px 0px 0px;
   height: 540px;
-  padding: 0 30px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 0 30px 30px 30px;
   overflow: hidden;
 `
 
 const Row = styled.div`
-  padding: 20px 0 30px 0;
+  padding: 24px 0 0 0;
   position: relative;
-`
-
-const Heading = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-  position: relative;
-  height: 30px;
 `
 
 const Title = styled.p`
@@ -37,15 +27,16 @@ const Title = styled.p`
   font-size: 20px;
   line-height: 23px;
   color: #1d1d22;
+  text-align: center;
 `
 
 const Receive = styled.div`
   width: 100%;
-  height: 300px;
+  height: 290px;
   background-color: #ffffff;
   border-radius: 16px;
   padding: 30px 38px 0 38px;
-  margin: 20px 0 0 0;
+  margin: 13px 0 0 0;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -68,28 +59,16 @@ const Warning = styled.p`
   color: #7d7e8d;
 `
 
-const MoreButton = styled.div`
-  width: 30px;
-  height: 30px;
-  background: rgba(222, 225, 233, 0.6);
-  border-radius: 15px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: absolute;
-  right: 0;
-
-  path {
-    fill: #7d7e8d;
-  }
+const GenerateExtraId = styled.p`
+  margin: 5px 0 0 0;
+  font-size: 14px;
+  line-height: 16px;
+  text-align: center;
+  color: #3fbb7d;
 
   &:hover {
     cursor: pointer;
-    background: rgba(222, 225, 233, 0.4);
-
-    path {
-      fill: #3fbb7d;
-    }
+    text-decoration: underline;
   }
 `
 
@@ -97,12 +76,11 @@ const Styles = {
   Wrapper,
   Container,
   Row,
-  Heading,
   Title,
   Receive,
   Address,
   Warning,
-  MoreButton,
+  GenerateExtraId,
 }
 
 export default Styles
