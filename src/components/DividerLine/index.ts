f7826438227b62e6353ed26ae1d398ecba3ec8a9
Lucas Root <lucas@simpleswap.io>
2021-08-30 12:59:12 +0300

    feat: add DividerLine component

---
+++ src/components/DividerLine/index.ts
@@ -0,0 +1,3 @@
+import DividerLine from './DividerLine'
+
+export default DividerLine
---
+++ src/components/DividerLine/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+const Container = styled.div`
+  width: 100%;
+  height: 1px;
+  background-color: rgba(222, 225, 233, 0.5);
+  margin-left: 30px;
+`
+
+const Styles = {
+  Container,
+}
+
+export default Styles
