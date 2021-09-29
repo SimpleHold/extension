eaf322f5a383e58b826b8ceca29332e72636d6c9
Lucas Root <lucas@simpleswap.io>
2021-09-29 17:28:18 +0300

    refactor: NFT page

--- src/pages/Nft/Nft.tsx
+++ src/pages/Nft/Nft.tsx
@@ -1,9 +1,23 @@
 import * as React from 'react'
 import { useHistory, useLocation } from 'react-router-dom'
+import SVG from 'react-inlinesvg'
 
 // Components
 import Cover from '@components/Cover'
 import Header from '@components/Header'
+import Button from '@components/Button'
+
+// Drawers
+import AddImageDrawer from '@drawers/AddImage'
+
+// Utils
+import { getContractUrl } from '@utils/currencies'
+import { openWebPage } from '@utils/extension'
+import { short } from '@utils/format'
+
+// Assets
+import linkIcon from '@assets/icons/link.svg'
+import emptyNftImage from '@assets/icons/emptyNftImage.svg'
 
 // Styles
 import Styles from './styles'
@@ -18,49 +32,92 @@ interface ILocationState {
   name: string
   smartContract: string
   chain: string
-  image: string
+  image?: string
   traits?: TTrait[]
 }
 
 const NftPage: React.FC = () => {
   const history = useHistory()
   const {
-    state: { tokenId, name, smartContract, chain, image, traits },
+    state,
+    state: { tokenId, name, smartContract, chain, traits },
   } = useLocation<ILocationState>()
 
+  const [activeDrawer, setActiveDrawer] = React.useState<null | 'addImage'>(null)
+  const [image, setImage] = React.useState<undefined | string>(state.image)
+
+  const onAddImage = (): void => {
+    setActiveDrawer('addImage')
+  }
+
+  const onViewContact = (): void => {
+    openWebPage(getContractUrl(smartContract, chain))
+  }
+
+  const onCloseDrawer = (): void => {
+    setActiveDrawer(null)
+  }
+
+  const updateImage = (newImage: string) => {
+    setImage(newImage)
+    setActiveDrawer(null)
+  }
+
+  const onErrorImageLoad = (): void => {
+    setImage(undefined)
+  }
+
   return (
-    <Styles.Wrapper>
-      <Cover />
-      <Header withBack onBack={history.goBack} backTitle="Wallet" />
-      <Styles.Container>
-        <Styles.Title>
-          {name} {tokenId}
-        </Styles.Title>
-        <Styles.SubTitle>{name}</Styles.SubTitle>
+    <>
+      <Styles.Wrapper>
+        <Cover />
+        <Header withBack onBack={history.goBack} backTitle="Wallet" />
+        <Styles.Container>
+          <Styles.Title>
+            {name} {tokenId}
+          </Styles.Title>
+          <Styles.SubTitle>{name}</Styles.SubTitle>
+          {image ? (
+            <Styles.Image src={image} alt="image" onError={onErrorImageLoad} />
+          ) : (
+            <Styles.EmptyImageRow>
+              <Styles.EmptyImage src={emptyNftImage} alt="image" />
+            </Styles.EmptyImageRow>
+          )}
 
-        <Styles.ContractInfo>
-          <Styles.ContractLabel>Smart Contract:</Styles.ContractLabel>
-          <Styles.ContractLink>{smartContract}</Styles.ContractLink>
-        </Styles.ContractInfo>
+          {traits?.length ? (
+            <Styles.Traits>
+              {traits.map((trait: TTrait) => {
+                const { trait_type, value } = trait
 
-        <Styles.Art src={image} alt="art" />
+                return (
+                  <Styles.Trait key={trait_type}>
+                    <Styles.TraitValue>{`${trait_type}: ${value}`}</Styles.TraitValue>
+                  </Styles.Trait>
+                )
+              })}
+            </Styles.Traits>
+          ) : null}
 
-        {traits?.length ? (
-          <Styles.Traits>
-            {traits.map((trait: TTrait) => {
-              const { trait_type, value } = trait
+          <Styles.ContractBlock>
+            <Styles.ContractBlockRow>
+              <Styles.ContractBlockLabel>Smart Contract</Styles.ContractBlockLabel>
+              <Styles.ContractBlockLink>{short(smartContract, 25)}</Styles.ContractBlockLink>
+            </Styles.ContractBlockRow>
+            <Styles.ContractButton onClick={onViewContact}>
+              <SVG src={linkIcon} width={12} height={12} />
+            </Styles.ContractButton>
+          </Styles.ContractBlock>
 
-              return (
-                <Styles.Trait key={trait_type}>
-                  <Styles.TraitType>{trait_type}</Styles.TraitType>
-                  <Styles.TraitValue>{value}</Styles.TraitValue>
-                </Styles.Trait>
-              )
-            })}
-          </Styles.Traits>
-        ) : null}
-      </Styles.Container>
-    </Styles.Wrapper>
+          <Button label="Add image" mt={20} onClick={onAddImage} />
+        </Styles.Container>
+      </Styles.Wrapper>
+      <AddImageDrawer
+        isActive={activeDrawer === 'addImage'}
+        onClose={onCloseDrawer}
+        updateImage={updateImage}
+      />
+    </>
   )
 }
 
--- src/pages/Nft/styles.ts
+++ src/pages/Nft/styles.ts
@@ -9,7 +9,7 @@ const Container = styled.div`
   background-color: #ffffff;
   border-radius: 16px 16px 0 0;
   height: 540px;
-  padding: 20px 30px;
+  padding: 20px 30px 30px 30px;
   overflow: scroll;
 `
 
@@ -22,69 +22,108 @@ const Title = styled.p`
 `
 
 const SubTitle = styled.p`
-  margin: 4px 0 0 0;
+  margin: 3px 0 0 0;
   font-size: 16px;
   line-height: 19px;
   color: #7d7e8d;
 `
 
-const ContractInfo = styled.div`
-  margin: 10px 0 0 0;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-`
-
-const ContractLabel = styled.p`
-  margin: 0;
-  font-weight: 500;
-  font-size: 14px;
-  line-height: 16px;
-  color: #bdc4d4;
-`
-
-const ContractLink = styled.p`
-  margin: 0 0 0 2px;
-  font-weight: 500;
-  font-size: 14px;
-  line-height: 16px;
-  color: #1d1d22;
-`
-
-const Art = styled.img`
+const EmptyImageRow = styled.div`
+  margin: 20px 0 0 0;
   width: 100%;
-  height: auto;
-  object-fit: contain;
+  height: 315px;
+  background-color: #f2f4f8;
   border-radius: 16px;
-  margin: 23px 0 0 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `
 
 const Traits = styled.div`
   display: flex;
   flex-wrap: wrap;
-  margin: 20px -5px 0 -5px;
+  margin: 14px -3.5px 0 -3.5px;
 `
 
 const Trait = styled.div`
-  display: flex;
-  flex-direction: row;
-  margin: 0 5px 5px 5px;
-`
-
-const TraitType = styled.p`
-  margin: 0;
-  font-weight: 500;
-  font-size: 16px;
-  line-height: 19px;
-  color: #bdc4d4;
+  padding: 7px 10px;
+  background: #f2f4f8;
+  border-radius: 8px;
+  margin: 6px 3.5px 0 3.5px;
 `
 
 const TraitValue = styled.p`
   margin: 0;
+  font-size: 14px;
+  line-height: 16px;
+  color: #1d1d22;
+`
+
+const ContractBlock = styled.div`
+  padding: 15px 15px 15px 20px;
+  filter: drop-shadow(0px 2px 20px rgba(125, 126, 141, 0.1));
+  background-color: #ffffff;
+  border-radius: 16px;
+  margin: 20px 0 0 0;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`
+
+const ContractBlockRow = styled.div`
+  flex: 1;
+  margin: 0 16px 0 0;
+`
+
+const ContractBlockLabel = styled.p`
+  margin: 0;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 16px;
+  color: #bdc4d4;
+`
+
+const ContractBlockLink = styled.p`
+  margin: 5px 0 0 0;
   font-weight: 500;
   font-size: 16px;
   line-height: 19px;
-  color: #bdc4d4;
+  color: #1d1d22;
+`
+
+const ContractButton = styled.div`
+  width: 30px;
+  height: 30px;
+  border-radius: 15px;
+  background-color: #f2f4f8;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  path {
+    fill: #bdc4d4;
+  }
+
+  &:hover {
+    cursor: pointer;
+
+    path {
+      fill: #3fbb7d;
+    }
+  }
+`
+
+const EmptyImage = styled.img`
+  width: 126px;
+  height: 126px;
+`
+
+const Image = styled.img`
+  width: 100%;
+  height: 315px;
+  object-fit: contain;
+  margin: 20px 0 0 0;
+  border-radius: 16px;
 `
 
 const Styles = {
@@ -92,14 +131,17 @@ const Styles = {
   Container,
   Title,
   SubTitle,
-  ContractInfo,
-  ContractLabel,
-  ContractLink,
-  Art,
+  EmptyImageRow,
   Traits,
   Trait,
-  TraitType,
   TraitValue,
+  ContractBlock,
+  ContractBlockRow,
+  ContractBlockLabel,
+  ContractBlockLink,
+  ContractButton,
+  EmptyImage,
+  Image,
 }
 
 export default Styles
