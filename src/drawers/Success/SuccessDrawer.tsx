412485f41574a750a7b09a1a2c9a8ce7008c5805
Dylan-Simplehold <dylan@simpleswap.io>
2021-10-26 11:43:34 +0300

    halloween sendIcon fix

--- src/drawers/Success/SuccessDrawer.tsx
+++ src/drawers/Success/SuccessDrawer.tsx
@@ -17,10 +17,11 @@ interface Props {
   disableClose?: boolean
   icon?: string
   isCloseOnLinkClick?: boolean
+  halloween?: boolean
 }
 
 const SuccessDrawer: React.FC<Props> = (props) => {
-  const { isActive, onClose, text, link, openFrom, disableClose, icon, isCloseOnLinkClick } = props
+  const { isActive, onClose, text, link, openFrom, disableClose, icon, isCloseOnLinkClick, halloween } = props
 
   const openLink = async (): Promise<void> => {
     await browser.tabs.create({
@@ -39,6 +40,7 @@ const SuccessDrawer: React.FC<Props> = (props) => {
       onClose={onClose}
       icon={icon || '../../assets/drawer/success.svg'}
       openFrom={openFrom}
+      halloween={halloween}
     >
       <Styles.Row>
         {text ? <Styles.Text>{text}</Styles.Text> : null}
--- src/pages/SendConfirmation/SendConfirmation.tsx
+++ src/pages/SendConfirmation/SendConfirmation.tsx
@@ -327,6 +327,7 @@ const SendConfirmation: React.FC = () => {
         onClose={closeSuccessDrawer}
         text="Your transaction has been successfully sent. You can check it here:"
         link={state.transactionLink}
+        halloween={true}
       />
 
       <FailDrawer
