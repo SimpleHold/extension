1d89958b6a1411ce2a4a9661a12e8e7fa32318d5
Dylan-Simplehold <92319334+Dylan-Simplehold@users.noreply.github.com>
2023-01-26 20:30:14 +0800

    fix: add token ticker

--- src/pages/NewWallet/NewWallet.tsx
+++ src/pages/NewWallet/NewWallet.tsx
@@ -83,10 +83,9 @@ const NewWallet: React.FC = () => {
       decimals,
     })
   }
-
   const getCurrenciesList = (getCurrencyInfo?: TCurrency | undefined | null): string[] => {
     if (getCurrencyInfo) {
-      return getList(getCurrencyInfo.symbol, chain)
+      return getList(symbol, chain)
     }
 
     return [symbol]
